65cba25f91f46b2f13f59c873cd2842026a07fdd
Cris-Mur <yomy98@gmail.com>
2020-10-29 23:05:48 -0500

    [no path]

--- client/server.js
+++ client/server.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
-const publicPath = path.join(__dirname, '..', 'public');
-
-app.use(express.static(publicPath));
+app.use(express.static('/'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile('index.html');
 });
 app.listen(process.env.PORT || 8080);
