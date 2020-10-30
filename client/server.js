1af2da53b3fbf9d214d010ee3e336b60c254dc82
Cris-Mur <yomy98@gmail.com>
2020-10-29 23:34:30 -0500

    [react docs]

--- client/server.js
+++ client/server.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.use(express.static('/build/'));
-app.get('/*', function(req, res) {
-  res.sendFile('index.html');
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+app.listen(9000);
